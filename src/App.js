5c5b4dba3a0d24d97cc6d61fda277cb4c5f9c516
Trishna Patel <trishnamorar@gmail.com>
2023-04-05 10:09:43 -0500

    updated CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href=" " target="_blank">
+            Trish Patel
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
