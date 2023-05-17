744ea96c593915de81652956342dc6c82ad48de1
Trishna Patel <trishnamorar@gmail.com>
2023-05-17 14:28:48 -0500

    Import API

--- src/Weathericon.js
+++ src/Weathericon.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weathericon(props) {
-    const codeMapping = {
+  const codeMapping = {
     "01d": "CLEAR_DAY",
     "01n": "CLEAR_NIGHT",
     "02d": "PARTLY_CLOUDY_DAY",
@@ -31,4 +31,4 @@ export default function Weathericon(props) {
       animate={true}
     />
   );
-},
+}
