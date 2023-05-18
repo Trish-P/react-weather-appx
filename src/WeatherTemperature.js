320719a8b3a38c3bb6df015fee5bc1a270a52e5e
Trishna Patel <trishnamorar@gmail.com>
2023-05-18 11:42:51 -0500

    updated days on weather forecast

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,34 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-
   if (unit === "celcius") {
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celcius)}</span>
-        <span className="unit">
-          °C |<p onClick={showFahrenheit}>°F</p>{" "}
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celcius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <p onClick={showCelcius}>°C |</p> °F{" "}
-        </span>
+        <span className="unit">°C</span>
       </div>
     );
   }
